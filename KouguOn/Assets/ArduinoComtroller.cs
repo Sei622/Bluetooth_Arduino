3b2aad57423d93f57befb9f384340545e4983b1d
Sei622 <mashiko622@gmail>
2020-10-27 23:21:26 +0900

    双方向通信

--- KouguOn/Assets/ArduinoComtroller.cs
+++ KouguOn/Assets/ArduinoComtroller.cs
@@ -6,26 +6,25 @@ using UnityEngine.UI;
 
 public class ArduinoComtroller : MonoBehaviour
 {
-    private Text m_DistanceText; //距離の表示するテキスト
     private SerialPort m_SerialPort; //USBポート
 
     void Start()
     {
-        m_DistanceText = FindObjectOfType<Text>();　//シンでのテキストを探す
         m_SerialPort = new SerialPort("COM5", 9600);　//ポートを指定する
-        m_SerialPort.Open();　//ポートを開く
+        m_SerialPort.Open(); //ポートを開く
+
+        if (m_SerialPort.IsOpen) // Arduinoと接続することを確認
+        {
+            Debug.Log("Serial通信成功");
+        }
     }
 
     void Update()
     {
-        if (m_SerialPort.IsOpen) // Arduinoと接続することを確認
+        if (Input.GetKeyDown(KeyCode.H)) // Arduinoと接続することを確認
         {
-            string data = m_SerialPort.ReadLine();　// Arduinoからのデータを取得する
-            float dis = 0;
-            if (float.TryParse(data, out dis)) //Arduinoからのデータは数かどうか確認する
-            {
-                m_DistanceText.text = dis + "cm";　//画面に表示する
-            }
+            Debug.Log("送信");
+            m_SerialPort.Write("Hello");
         }
     }
     private void OnApplicationQuit()
